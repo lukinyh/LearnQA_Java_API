85c90b8544c85ed8e40094f4e861bf4991bd06b8
testUser <test@gmail.com>
2022-12-16 17:32:43 +0300

    Ex10: added function that checked short phrase

---
+++ src/test/java/tests/homework2/TestShortPhrase.java
@@ -0,0 +1,18 @@
+package tests.homework2;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+* These tests check length variable (String).
+* */
+public class TestShortPhrase {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "123", "15 symbols: abc", "More than fifteen symbols"})
+    public void testShortPhrase(String var) {
+        assertTrue(var.length() > 15, "Length of variable '" + var + "' has less or equal 15 symbols");
+    }
+}
